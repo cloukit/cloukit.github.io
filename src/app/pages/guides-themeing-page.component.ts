55b9024d7154ad2b82caf78a6ac6de0d3cfe883d
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-07-04 18:05:19 +0200

    doc

--- src/app/pages/guides-themeing-page.component.ts
+++ src/app/pages/guides-themeing-page.component.ts
@@ -19,6 +19,48 @@ import { GuidesDemoData } from '../model/guides-demo-data.model';
       But first we need to understand how cloukit components are composed and how styling is applied.
     </p>
     <h3>Decomposing the toggle Component</h3>
+    <p>
+      At first I want to mention that all cloukit components <strong>know about their UI state</strong>.
+      What do I mean by that? If you in the "traditional" way of styling with CSS would give
+      an element an <code>el:hover</code> style your JS code would not know that a mouseover changes the styling.
+      Cloukit elements know about their UI state because it is handled with JS. 
+      CSS is only applied directly for an element and <a href="https://developer.mozilla.org/en/docs/Web/CSS/Pseudo-classes">
+      pseudo-classes</a> are not used.      
+    </p>
+    <p>
+      Therefore cloukit uses the following wording:
+    </p>
+    <table>
+      <tr>
+        <td style="vertical-align:top"><strong>Component</strong></td>
+        <td>is a cloukit Angular Component like like toggle.</td>
+      </tr>
+      <tr>
+        <td style="vertical-align:top"><strong>Element</strong></td>
+        <td>is a DOM-element like <code>div</code>, <code>span</code> or <code>svg</code> inside the component.</td>
+      </tr>
+      <tr>
+        <td style="vertical-align:top"><strong>UI State</strong></td>
+        <td>
+          is the generic state your component is in regarding its UI. A toggle can be <em>toggled</em> or
+          <em>untoggled</em>. A textfield could be <em>focused</em> or <em>unfocused</em>. The UI State depends on the component type.
+        </td>
+      </tr>
+      <tr>
+        <td style="vertical-align:top"><strong>UI Modifier</strong></td>
+        <td>
+        is the generic modifier for the state your component is in regarding its UI. 
+        While a toggle could be in its UI State <em>toggled</em> it still could be hovered with a mouse
+         and this would then for example be the UI modifier <em>hover</em>. Or if the form is disabled
+         the UI modifier would be <em>disabled</em>.
+        </td>
+      </tr>
+    </table>
+    <p>
+      A <strong>cloukit Theme</strong> holds styling information for all elements
+      of a component with all permutations of UI States and UI Modifiers. Below you see the example for
+      the toggle component.
+    </p>
     <p style="text-align: center">
       <img src="/assets/images/guide/toggle-component-decomposed.svg" width="80%" />
     </p>
